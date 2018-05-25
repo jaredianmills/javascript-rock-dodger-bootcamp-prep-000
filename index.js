e3acb2f66c9e7e12dc9b476b3af1acaf3c8cbdb3
jaredianmills <jaredianmills@gmail.com>
2018-05-25 17:49:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,33 +78,23 @@ function moveDodger(e) {
 
 function moveDodgerLeft() {
   let left = positionToInteger(DODGER.style.left);
-  if (left < 4) {
-    DODGER.style.left = '0px';
-    return;
-  } else {
   function step() {
     DODGER.style.left = `${left -= 4}px`;
     if (left > 0) {
       window.requestAnimationFrame(step);
     }
   }
-}
   window.requestAnimationFrame(step);
 }
 
 function moveDodgerRight() {
   let right = positionToInteger(DODGER.style.left);
-  if (right > 356) {
-    DODGER.style.left = '360px';
-    return;
-  } else {
   function step() {
     DODGER.style.left = `${right += 4}px`;
     if (right < 360) {
       window.requestAnimationFrame(step);
     }
   }
-}
   window.requestAnimationFrame(step);
 }
 
